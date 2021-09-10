ce2559baf6f97dc98d79837f6bfaad91f21e56fb
Satsukevich Dzmitry <dimamoving@gmail.com>
2021-09-10 15:19:07 +0300

    fix merge conflict

--- src/app/components/headerAndFooter/header/header.jsx
+++ src/app/components/headerAndFooter/header/header.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-<<<<<<< HEAD:src/app/components/headerAndFooter/header.jsx
-export default function Header(props) {
-=======
 const Header = () => {
->>>>>>> 23e0d79a7e2d28534e79c314971cfb96a018be2a:src/app/components/headerAndFooter/header/header.jsx
 	const user = useSelector((state) => state.user);
 	const { logged } = user;
 	return (
@@ -25,13 +21,9 @@ const Header = () => {
 					<input type="text" className="navbar_seach-input" />
 				</nav>
 				{logged ? (
-<<<<<<< HEAD:src/app/components/headerAndFooter/header.jsx
-					<p>{JSON.stringify(user)}</p>
-=======
 					<Link to="/" className="navbar-link">
 						{user.user.email}
 					</Link>
->>>>>>> 23e0d79a7e2d28534e79c314971cfb96a018be2a:src/app/components/headerAndFooter/header/header.jsx
 				) : (
 					<section className="header_auth-section">
 						<Link to="/logIn" className="header_auth-section_link">
--- src/app/redux/reduxSaga/sagas.js
+++ src/app/redux/reduxSaga/sagas.js
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-import { takeEvery, put, call, all } from 'redux-saga/effects';
-import getData from '../../services/getData';
-import { GET_REQUEST, GET_SUCCESS } from '../booksSlice/booksSlice';
-
-function* fetchBooksSaga() {
-	const payload = yield call(getData);
-	yield put(GET_SUCCESS(payload));
-}
-
-function* sagaWatcher() {
-	yield takeEvery(GET_REQUEST, fetchBooksSaga);
-};
-=======
 import { all } from 'redux-saga/effects';
 import userLogInSagaWatcher from './usersSaga/userSaga';
 import bookSagaWatcher from './booksSaga/booksSaga';
 
->>>>>>> 23e0d79a7e2d28534e79c314971cfb96a018be2a
 export default function* rootSaga() {
 	yield all([bookSagaWatcher(), userLogInSagaWatcher()]);
 }
